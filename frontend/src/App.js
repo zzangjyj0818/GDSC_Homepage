2f926b164548fb56a256abfc442f394c0a63c96d
zzangjyj0818 <jyj010818@naver.com>
2023-11-12 16:34:05 +0900

    setting the initial state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'styled-components'
+
+const ScrollSnapWrap = styled.div`
+  scroll-snap-type: y mandatory;
+  height: 100vh;
+  overflow-y: scroll;
+`;
+
+const SnapDiv = styled.div`
+  scroll-snap-align: start;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  border: 5px solid red;
+  text-align: center;
+  font-size: 100px;
+`;
 
 const App = () => {
   return (
-    <div>
-      frontend
-    </div>
+    <>
+      <ScrollSnapWrap>
+        <SnapDiv>1</SnapDiv>
+        <SnapDiv>2</SnapDiv>
+        <SnapDiv>3</SnapDiv>
+        <SnapDiv>4</SnapDiv>
+        <SnapDiv>5</SnapDiv>
+      </ScrollSnapWrap>
+    </>
   );
 };
 
